b581cff6f081743016df3ff82c10b8de208141c0
Amrrish1209 <roshanbabubl@gmail.com>
2023-05-23 14:46:02 +0530

    Add: Ability to ensure Valid Contacts are added...

--- addressbook.js
+++ addressbook.js
@@ -12,6 +12,49 @@ function addContact(
   phoneNumber,
   email
 ) {
+  // Validation regular expressions
+  var nameRegex = /^[A-Z][a-zA-Z]{2,}$/;
+  var addressRegex = /^.{4,}$/;
+  var zipRegex = /^\d{5}$/;
+  var phoneRegex = /^\d{3}-\d{3}-\d{4}$/;
+  var emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+
+  // Validate first name and last name
+  if (!nameRegex.test(firstName) || !nameRegex.test(lastName)) {
+    throw new Error(
+      "First name and last name should start with a capital letter and have a minimum of 3 characters."
+    );
+  }
+
+  // Validate address, city, and state
+  if (
+    !addressRegex.test(address) ||
+    !addressRegex.test(city) ||
+    !addressRegex.test(state)
+  ) {
+    throw new Error(
+      "Address, city, and state should have a minimum of 4 characters."
+    );
+  }
+
+  // Validate zip code
+  if (!zipRegex.test(zip)) {
+    throw new Error("Invalid ZIP code.");
+  }
+
+  // Validate phone number
+  if (!phoneRegex.test(phoneNumber)) {
+    throw new Error(
+      "Invalid phone number. Please use the format: XXX-XXX-XXXX."
+    );
+  }
+
+  // Validate email
+  if (!emailRegex.test(email)) {
+    throw new Error("Invalid email address.");
+  }
+
+  // Create the contact object and add it to the address book
   var contact = {
     firstName: firstName,
     lastName: lastName,
@@ -46,15 +89,18 @@ function displayContacts() {
   }
 }
 
-addContact(
-  "Amrrish",
-  "Roshan",
-  "123 Main St",
-  "Nagercoil",
-  "TamilNadu",
-  "10001",
-  "123456789",
-  "Amrrish@example.com"
-);
-
-displayContacts();
+try {
+  addContact(
+    "Amrrish",
+    "Roshan",
+    "123 Main St",
+    "Nagercoil",
+    "TamilNadu",
+    "10001",
+    "123-456-7890",
+    "Amrrish@example.com"
+  );
+  displayContacts();
+} catch (error) {
+  console.log("Error adding contact:", error.message);
+}
